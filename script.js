61c4e54366ccacb71f0c8e30e67fe5da77eea407
Roger Martinez <rogeraaronmartinez@gmail.com>
2023-11-14 10:46:44 -0600

    Added functionality required to allow user to input grid size and update DOM

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-const gridContainer = document.getElementById('gridContainer');
+var gridContainer = document.getElementById('gridContainer');
 
 
-for (let i=0; i<16; i++) {
-    for (let j=0; j<16; j++) {
-    const square = document.createElement('div');
 
-    square.classList.add('grid-item');
+function makeGrid(input) {
+   
+    for (let i=0; i<input; i++) {
+        for (let j=0; j<input; j++) {
+        const square = document.createElement('div');
+        square.classList.add('grid-item');
+        gridContainer.appendChild(square);
+        }
+    }
 
-    gridContainer.appendChild(square);
-}}
+    let newColor = document.querySelectorAll(".grid-item");
+    for (const color of newColor) {
+        color.addEventListener("mouseover", () => {
+            color.style.backgroundColor = "red";
+        })
+        
+    }
+}
 
+
+
+
+function gridSize() {
+    let grid = prompt("Enter grid size up to 100");
+    if(grid < 100) {
+        let num = 22*grid;
+        gridContainer.style.height = num + "px";
+        gridContainer.style.width = num + "px";
+        makeGrid(grid);
+    }
+}
